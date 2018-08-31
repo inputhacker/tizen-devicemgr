049bfe9fc9e3e1a86f3a4f8a2a5a3463c8f2a114
JengHyun Kang <jhyuni.kang@samsung.com>
2018-08-31 15:53:23 +0900

    remove input functions in devicemgr
    Change-Id: I3eff0c5dcf3e8b95a80b6c9ba137afc4ccbf4cab

--- packaging/e-mod-tizen-devicemgr.spec
+++ packaging/e-mod-tizen-devicemgr.spec
@@ -8,11 +8,8 @@ Source0: %{name}-%{version}.tar.gz
 License: BSD-2-Clause
 BuildRequires: pkgconfig(enlightenment)
 BuildRequires: pkgconfig(dlog)
-BuildRequires: pkgconfig(ttrace)
 BuildRequires: pkgconfig(wayland-server)
 BuildRequires: pkgconfig(tizen-extension-server)
-BuildRequires: pkgconfig(cynara-client)
-BuildRequires: pkgconfig(cynara-creds-socket)
 
 
 %global TZ_SYS_RO_SHARE  %{?TZ_SYS_RO_SHARE:%TZ_SYS_RO_SHARE}%{!?TZ_SYS_RO_SHARE:/usr/share}
@@ -29,7 +26,7 @@ export GC_SECTIONS_FLAGS="-fdata-sections -ffunction-sections -Wl,--gc-sections"
 export CFLAGS+=" -Wall -Werror -g -fPIC -rdynamic ${GC_SECTIONS_FLAGS} -DE_LOGGING=1"
 export LDFLAGS+=" -Wl,--hash-style=both -Wl,--as-needed -Wl,--rpath=/usr/lib"
 
-%reconfigure --enable-wayland-only --enable-cynara
+%reconfigure --enable-wayland-only
 
 make
 
--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -1,14 +1,8 @@
 #include "e.h"
+#include "dlog.h"
 #include "eina_log.h"
 #include "e_mod_main.h"
-#include "e_devicemgr_input.h"
-#ifdef HAVE_WAYLAND_ONLY
 #include "e_devicemgr_embedded_compositor.h"
-#include "e_devicemgr_device.h"
-#endif
-#include "e_devicemgr_privates.h"
-
-int _log_dom = -1;
 
 /* this is needed to advertise a label for the module IN the code (not just
  * the .desktop file) but more specifically the api version it was compiled
@@ -20,8 +14,6 @@ EAPI E_Module_Api e_modapi =
    "DeviceMgr Module of Window Manager"
 };
 
-E_Devicemgr_Config_Data *dconfig;
-
 EAPI void *
 e_modapi_init(E_Module *m)
 {
@@ -31,64 +23,19 @@ e_modapi_init(E_Module *m)
         return NULL;
      }
 
-   _log_dom = eina_log_domain_register("e-devicemgr", EINA_COLOR_BLUE);
-   if (_log_dom < 0)
-     {
-        SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ eina_log_domain_register()..!\n", __FUNCTION__);
-        return NULL;
-     }
-   eina_log_domain_level_set("e-devicemgr", EINA_LOG_LEVEL_INFO);
-
-   dconfig = E_NEW(E_Devicemgr_Config_Data, 1);
-   if (!dconfig)
-     {
-        SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ allocate memory for config data\n", __FUNCTION__);
-        return NULL;
-     }
-   dconfig->module = m;
-   e_devicemgr_conf_init(dconfig);
-   if (!dconfig->conf)
-     {
-        SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ get config data\n", __FUNCTION__);
-        return NULL;
-     }
-
-   if (!e_devicemgr_input_init())
-     {
-        SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ e_devicemgr_input_init()..!\n", __FUNCTION__);
-        return NULL;
-     }
-
-#ifdef HAVE_WAYLAND_ONLY
    if (!e_devicemgr_embedded_compositor_init())
      {
         SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ e_devicemgr_embedded_compositor_init()..!\n", __FUNCTION__);
         return NULL;
      }
 
-   if (!e_devicemgr_device_init())
-     {
-        SLOG(LOG_DEBUG, "DEVICEMGR", "[e_devicemgr][%s] Failed @ e_devicemgr_device_init()..!\n", __FUNCTION__);
-        return NULL;
-     }
-#endif
-
-   return dconfig;
+   return m;
 }
 
 EAPI int
 e_modapi_shutdown(E_Module *m)
 {
-   E_Devicemgr_Config_Data *dconf = m->data;
-#ifdef HAVE_WAYLAND_ONLY
    e_devicemgr_embedded_compositor_fini();
-   e_devicemgr_device_fini();
-#endif
-   e_devicemgr_input_fini();
-   e_devicemgr_conf_fini(dconf);
-   E_FREE(dconf);
-
-   eina_log_domain_unregister(_log_dom);
    eina_shutdown();
 
    return 1;
@@ -98,9 +45,5 @@ EAPI int
 e_modapi_save(E_Module *m)
 {
    /* Do Something */
-   E_Devicemgr_Config_Data *dconf = m->data;
-   e_config_domain_save("module.devicemgr",
-                        dconf->conf_edd,
-                        dconf->conf);
    return 1;
 }
